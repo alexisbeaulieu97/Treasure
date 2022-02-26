1d5064de2328c41db8d0e63f0776f84d07ec9d09
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2022-02-26 02:04:08 -0500

    Added typing to key.py

--- src/key.py
+++ src/key.py
@@ -20,14 +20,23 @@ class Key:
             "mem": pwhash.argon2i.MEMLIMIT_SENSITIVE,
         }
 
-    def __init__(self, password=None, salt=None, securityLevel=SecurityLevel.HIGH):
+    def __init__(
+        self,
+        password: str = None,
+        salt: bytes = None,
+        securityLevel: dict = SecurityLevel.HIGH,
+    ):
         password = rand.gen_password() if password is None else password
         self.salt = rand.gen_salt() if salt is None else salt
         self.securityLevel = securityLevel
         self.value = Key.derive(password, self.salt, self.securityLevel)
 
     @staticmethod
-    def derive(password, salt=None, securityLevel=SecurityLevel.HIGH):
+    def derive(
+        password: str,
+        salt: bytes = None,
+        securityLevel: dict = SecurityLevel.HIGH,
+    ):
         salt = rand.gen_salt() if salt is None else salt
         return pwhash.argon2i.kdf(
             KEY_SIZE,
@@ -38,5 +47,10 @@ class Key:
         )
 
     @classmethod
-    def from_file(cls, salt_filepath, password=None, securityLevel=SecurityLevel.HIGH):
+    def from_file(
+        cls,
+        salt_filepath: str,
+        password: str = None,
+        securityLevel: dict = SecurityLevel.HIGH,
+    ):
         return cls(password, get_salt(salt_filepath), securityLevel)
